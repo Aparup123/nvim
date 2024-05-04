894222a1fae70a16ee23b1970c1de04e260def4c
Aparup123 <84846776+Aparup123@users.noreply.github.com>
2024-05-04 23:00:01 +0530

    added autopairs and autotags

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,7 @@
 vim.opt.relativenumber=true
 vim.g.have_nerd_font=true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
+vim.opt.tabstop=2
+vim.opt.shiftwidth=2
 vim.g.mapleader=" "
 vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>")
 vim.api.nvim_set_option("clipboard","unnamed")
